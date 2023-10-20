1d89fb83e63f3b4027b0646b1c4667ab67677a40
emilienbld <98468414+emilienbld@users.noreply.github.com>
2023-10-20 10:13:00 +0200

    Add files via upload
    ca fonctionne la je mérite le bleu

--- C++/mainwindow.cpp
+++ C++/mainwindow.cpp
@@ -6,18 +6,14 @@
 #include <algorithm>
 #include <QMessageBox>
 
-//#include <mutex>
-
-//std::mutex mtx;
-
 // Définition de constantes pour les couleurs et la taille de la police
 const QString COULEUR_FOND = "#FF5733";
 const QString COULEUR_BOUTON = "#E8E1E1";
 const QString COULEUR_TEXTE = "white";
 const int TAILLE_POLICE = 16;
 
+// Initialisation du jeu de cartes
 Deck::Deck() {
-    // Initialisation du jeu de cartes
     QString noms[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi", "As"};
     int valeurs[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 
@@ -28,41 +24,26 @@ Deck::Deck() {
     }
 }
 
+// Mélange les cartes en utilisant le temps comme graine
 void Deck::melanger() {
-    // Mélange les cartes en utilisant le temps comme graine
     srand(static_cast<unsigned>(time(0)));
     std::random_shuffle(cartes.begin(), cartes.end());
 }
 
+// Tire une carte du dessus du jeu
 Carte Deck::tirer_carte() {
-    // Tire une carte du dessus du jeu
     Carte carte = cartes.back();
     cartes.pop_back();
     return carte;
 }
 
-
-//Carte Deck::tirer_carte() {
-//    // Verrouiller le jeu de cartes
-//    std::lock_guard<std::mutex> lock(mtx);
-
-//    // Tirer une carte du dessus du jeu
-//    Carte carte = cartes.back();
-//    cartes.pop_back();
-
-//    // Déverrouiller le jeu de cartes lorsque la carte est tirée
-//    return carte;
-//}
-
-
-
+// Ajoute une carte à la main du joueur ou du croupier
 void Main::ajouter_carte(Carte carte) {
-    // Ajoute une carte à la main du joueur ou du croupier
     cartes.push_back(carte);
 }
 
+// Calcule la valeur totale des cartes dans la main
 int Main::calculer_valeur() {
-    // Calcule la valeur totale des cartes dans la main
     int valeur = 0;
     int nombre_as = 0;
     for (const Carte& carte : cartes) {
@@ -71,22 +52,21 @@ int Main::calculer_valeur() {
             nombre_as++;
         }
     }
+    // Réduit la valeur si un As provoque un dépassement de 21
     while (nombre_as > 0 && valeur > 21) {
-        valeur -= 10; // Réduit la valeur si un As provoque un dépassement de 21
+        valeur -= 10;
         nombre_as--;
     }
     return valeur;
 }
 
+// Initialisation de l'interface utilisateur et du jeu
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-    // Initialisation de l'interface utilisateur et du jeu
     ui->setupUi(this);
     this->setStyleSheet("background-color: " + COULEUR_FOND);
     ui->tirerButton->setStyleSheet("border-radius: 50px; background-color: " + COULEUR_BOUTON);
     ui->passerButton->setStyleSheet("border-radius: 50px; background-color: " + COULEUR_BOUTON);
 
-    connect(ui->tirerButton, SIGNAL(clicked()), this, SLOT(on_tirerButton_clicked()));
-    connect(ui->passerButton, SIGNAL(clicked()), this, SLOT(on_passerButton_clicked()));
     initialiserPartie();
 
     // Personnalisation du tapis, boutons et lables
@@ -104,14 +84,14 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 }
 
+// Configure le style des étiquettes de la main
 void MainWindow::setLabelStyles(QLabel* label) {
-    // Configure le style des étiquettes de la main
     label->setAlignment(Qt::AlignCenter);
     label->setStyleSheet("background-color: #66626B; color: " + COULEUR_TEXTE + "; font-size: " + QString::number(TAILLE_POLICE) + "px; border-radius: 20px;");
 }
 
+// Initialise une nouvelle partie en mélangeant le jeu et distribuant des cartes
 void MainWindow::initialiserPartie() {
-    // Initialise une nouvelle partie en mélangeant le jeu et distribuant des cartes
     deck.melanger();
     main_joueur = Main();
     main_croupier = Main();
@@ -122,52 +102,17 @@ void MainWindow::initialiserPartie() {
     afficher_main_croupier();
 }
 
+// Destructeur
 MainWindow::~MainWindow() {
-    // Destructeur
     delete ui;
 }
 
-//void MainWindow::on_tirerButton_clicked() {
-//    // Gère le clic sur le bouton "Tirer"
-//    main_joueur.ajouter_carte(deck.tirer_carte());
-//    afficher_main_joueur();
-//    int valeur_main_joueur = main_joueur.calculer_valeur();
-//    if (valeur_main_joueur > 21) {
-//        QString resultat = "Vous avez sauté (plus de 21 points). Le croupier gagne !";
-//            boiteMsg(resultat);
-//    }
-//}
-
-
-//void MainWindow::on_tirerButton_clicked() {
-//    // Gère le clic sur le bouton "Tirer"
-//    Carte carte_piochee = deck.tirer_carte();
-//    main_joueur.ajouter_carte(carte_piochee);
-
-//    // Afficher les cartes du joueur
-//    ui->mainJoueurLabel->setText(afficher_main_joueur() + "(" + QString::number(carte_piochee.valeur) + " points)");
-
-//    int valeur_main_joueur = main_joueur.calculer_valeur();
-//    if (valeur_main_joueur > 21) {
-//            QString resultat = "Vous avez sauté (plus de 21 points). Le croupier gagne !";
-//                boiteMsg(resultat);
-//    }
-//}
-
-
-
+// Gère le clic sur le bouton "Tirer"
 void MainWindow::on_tirerButton_clicked() {
-    // Gère le clic sur le bouton "Tirer"
-    Carte carte_piochee = deck.tirer_carte();
-    main_joueur.ajouter_carte(carte_piochee);
+    main_joueur.ajouter_carte(deck.tirer_carte());
 
     // Afficher les cartes du joueur
-    QString main_joueur_str = "";
-    for (const Carte& carte : main_joueur.obtenir_cartes()) {
-        main_joueur_str += carte.nom + " ";
-    }
-
-    ui->mainJoueurLabel->setText(main_joueur_str + "(" + QString::number(main_joueur.calculer_valeur()) + " points)");
+    afficher_main_joueur();
 
     int valeur_main_joueur = main_joueur.calculer_valeur();
     if (valeur_main_joueur > 21) {
@@ -176,15 +121,8 @@ void MainWindow::on_tirerButton_clicked() {
     }
 }
 
-
-
-
-
-
-
-
+// Gère le clic sur le bouton "Passer"
 void MainWindow::on_passerButton_clicked() {
-    // Gère le clic sur le bouton "Passer"
     while (main_croupier.calculer_valeur() < 17) {
         main_croupier.ajouter_carte(deck.tirer_carte());
     }
@@ -202,8 +140,8 @@ void MainWindow::on_passerButton_clicked() {
     boiteMsg(resultat);
 }
 
+// Affiche une boîte de dialogue avec le résultat et une option de rejouer ou quitter
 void MainWindow::boiteMsg(const QString& resultat) {
-    // Affiche une boîte de dialogue avec le résultat et une option de rejouer ou quitter
     QMessageBox messageBox;
     messageBox.setText(resultat);
     messageBox.setStandardButtons(QMessageBox::Retry | QMessageBox::Close);
@@ -216,8 +154,8 @@ void MainWindow::boiteMsg(const QString& resultat) {
     }
 }
 
+// Affiche la main du joueur et son total
 void MainWindow::afficher_main_joueur() {
-    // Affiche la main du joueur et sa valeur
     const std::vector<Carte>& cartes_joueur = main_joueur.obtenir_cartes();
     QString main_joueur_str = "";
     for (const Carte& carte : cartes_joueur) {
@@ -226,8 +164,8 @@ void MainWindow::afficher_main_joueur() {
     ui->mainJoueurLabel->setText(main_joueur_str + "(" + QString::number(main_joueur.calculer_valeur()) + " points)");
 }
 
+// Affiche la main du croupier et son total
 void MainWindow::afficher_main_croupier() {
-    // Affiche la main du croupier et sa valeur
     const std::vector<Carte>& cartes_croupier = main_croupier.obtenir_cartes();
     QString main_croupier_str = "";
     for (const Carte& carte : cartes_croupier) {
